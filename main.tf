f6ecf19aaca2d7df81b2cc081748f25cee23b4a9
keith.hand <43491549+keithhand@users.noreply.github.com>
2024-06-01 14:11:08 -0500

    build(pulumi): removes terraform files to prep pulumi

--- main.tf
+++
@@ -1,8 +0,0 @@
-module "unifi" {
-  source   = "./modules/unifi"
-  insecure = true
-  api_url  = var.credentials.unifi.api_url
-  username = var.credentials.unifi.username
-  password = var.credentials.unifi.password
-  network  = var.unifi.network
-}
--- modules/unifi/modules/network/providers.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    unifi = {
-      source  = "paultyng/unifi"
-      version = "~> 0.41"
-    }
-  }
-}
